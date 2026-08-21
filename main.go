d7a5943e0bc88d0ba632f7de28795c78afa79155
AmirAli <amirali.safie@gmail.com>
2026-08-21 12:27:42 -0500

    just wrote first function to establish connnection

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"port-scaner/send"
 )
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+	result, err := send.Send("127.0.0.1", "tcp", 631)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
 }
