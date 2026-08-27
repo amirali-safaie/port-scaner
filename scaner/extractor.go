d5bb0b37f70a75f851a7248a88d2617a07660b59
amirali <amirali.safie@gamil.com>
2026-08-27 18:01:05 +0330

    write basis of extractorsubhosts

--- scaner/extractor.go
+++ scaner/extractor.go
@@ -2,15 +2,26 @@ package scaner
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 
-func ExtractHosts(target string) ([]string,error) {
+func ListHosts(target string) ([]string,error) {
 	contain := strings.Contains(target,"/")
 	if contain {
 		return nil,nil
 	}else{
 		return []string{target},nil
 	}
+}
+
+func extractSubHosts(target string) ([]string,error) {
+	splitedTarget := strings.Split(target, "/")
+	CIDR, _ := strconv.Atoi(splitedTarget[1]) 
+	unChaingeAblePart := 4 - CIDR/8
+	IpParts := strings.Split(splitedTarget[0], ".")
+	baseIp := strings.Join(IpParts[:unChaingeAblePart], ".")
+	
+
 }
--- scaner/scan_host_test.go
+++ scaner/scan_host_test.go
@@ -7,7 +7,7 @@ import (
 
 
 func TestExtractor(t *testing.T) {
-	got, err := ExtractHosts("127.0.0.1/23")
+	got, err := ListHosts("127.0.0.1/23")
 	if err != nil{
 		fmt.Println(err)
 	}
