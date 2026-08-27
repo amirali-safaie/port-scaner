fff198b3b699ee12182c14117379f94c47416709
amirali <amirali.safie@gamil.com>
2026-08-27 20:31:20 +0330

    writing de2bi function to convert decimal into binary str

---
+++ utiles/utiles.go
@@ -0,0 +1,22 @@
+package utiles
+
+import (
+	"strconv"
+	"errors"
+)
+
+
+func dec2bi(num int) (string, error) {
+	if num < 0{
+		return "", errors.New("Negative number is not allowed")
+	}
+
+	binary := ""
+	for num > 0 {
+		mod := num%2
+		binary = strconv.Itoa(mod) + binary 
+		num = num/2
+	}
+
+	return binary, nil
+}	
