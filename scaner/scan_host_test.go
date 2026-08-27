88d6ef46e046046cd3bcc7e79a78de7757383ebe
AmirAli <amirali.safie@gmail.com>
2026-08-27 07:05:50 -0500

    add scaner dir (just commit t send in remote repo

---
+++ scaner/scan_host_test.go
@@ -0,0 +1,15 @@
+package scaner
+
+import (
+	"fmt"
+	"testing"
+)
+
+
+func TestExtractor(t *testing.T) {
+	got, err := ExtractHosts("127.0.0.1/23")
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Print(got)
+}
